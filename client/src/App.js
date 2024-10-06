777002b70f9f3c2516b85fc4fa5ee61f325632de
Fidelis Adang <fidenki1429@gmail.com>
2024-10-06 01:24:24 +0100

    homepage implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import crm from './images/crm_img.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header className='header'>
+        <div>JF-CRM</div>
+        <div>
+          <button className='button'>Get Started</button>
+        </div>
       </header>
+
+      <main className='main'>
+        <div className='main_cont'>
+          <h1 className='main_title'>
+            Meet JF-CRM – The World's Most Popular CRM
+          </h1>
+          <p className='main_desc'>
+            Over 15,000,000 businesses registered with JF-CRM since the launch
+            in 2022, making it one of the most widely used CRM solution in the
+            world.
+          </p>
+        </div>
+        <div className='centered'>
+          <button className='button_01'>Get Started</button>
+        </div>
+        <img src={crm} alt='crm chat' className='crm_chat' />
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
